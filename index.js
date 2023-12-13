cc3f18a82183b1a8bac6194fd3857460067a2a81
gadaman <randm2001@gmail.com>
2023-12-13 08:25:54 +0330

    Add past xorner points

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const x = 300;
 const y = 300;
-let d=300;
+let d = 200;
 let width = d;
 let height = d;
 let cx = x + width / 2;
 let cy = y + height / 2;
-let rotate = -20;
+let rotate = 0;
+
 
 function rotatePoint(cx, cy, x, y, angle) {
   var radians = (Math.PI / 180) * angle,
@@ -84,6 +85,16 @@ const origin4 = document.querySelector("#origin4");
 const orbit4 = document.querySelector("#orbit4");
 
 
+// svg = document.querySelector("svg");
+// let pt = svg.createSVGPoint();
+// console.log(pt);
+// var rotatedPoint = pt.matrixTransform(
+//   rect1.getScreenCTM().inverse()
+// );
+
+// console.log(rotatedPoint)
+
+
 rect1.setAttribute("x", x);
 rect1.setAttribute("y", y);
 rect1.setAttribute("width", width);
@@ -103,17 +114,16 @@ orbit1.setAttribute("r", calculateSquareDiagonal(width, height) / 2);
 let corners = getRectangleCorners(x, y, width, height);
 
 
-addSvgCircle(corners.topLeftX, corners.topLeftY, 5, "red");
-addSvgCircle(corners.topRightX, corners.topRightY, 5, "green");
-addSvgCircle(corners.bottomLeftX, corners.bottomLeftY, 5, "purple");
-addSvgCircle(corners.bottomRightX, corners.bottomRightY, 5, "blue");
+let pastLeftTopCircle = addSvgCircle(corners.topLeftX, corners.topLeftY, 5, "red");
+let pastRighttTopCircle = addSvgCircle(corners.topRightX, corners.topRightY, 5, "green");
+let pastLeftBottomCircle = addSvgCircle(corners.bottomLeftX, corners.bottomLeftY, 5, "purple");
+let pastRightBottomCircle = addSvgCircle(corners.bottomRightX, corners.bottomRightY, 5, "blue");
 
 
 let leftTopCircle = addSvgCircle(corners.topLeftX, corners.topLeftY, 5, "red");
 let righttTopCircle = addSvgCircle(corners.topRightX, corners.topRightY, 5, "green");
 let leftBottomCircle = addSvgCircle(corners.bottomLeftX, corners.bottomLeftY, 5, "purple");
 let rightBottomCircle = addSvgCircle(corners.bottomRightX, corners.bottomRightY, 5, "blue");
-
 
 const { newX: newTopLeftX, newY: newTopLeftY } = rotatePoint(cx, cy, corners.topLeftX, corners.topLeftY, rotate * -1);
 const { newX: newTopRightX, newY: newTopRightY } = rotatePoint(cx, cy, corners.topRightX, corners.topRightY, rotate * -1);
@@ -137,4 +147,30 @@ leftBottomCircle.setAttribute("cy", newBottomLeftY);
 
 rightBottomCircle.setAttribute("cx", newBottomRightX);
 rightBottomCircle.setAttribute("cy", newBottomRightY);
+
+
+// let moveMoventX=leftTopCircle.getAttribute("cx")-pastLeftTopCircle.getAttribute("cx");
+// let moveMoventY=leftTopCircle.getAttribute("cy")-pastLeftTopCircle.getAttribute("cy");
+
+//rect1.setAttribute("transform", `rotate(${0})`);
+
+// let newRectX=x+moveMoventX;
+// let newRectY=y+moveMoventY;
+
+// rect1.setAttribute("x", newRectX);
+// rect1.setAttribute("y", newRectY);
+
+// // let newCx = newRectX + width / 2;
+// // let newCy = newRectY + height / 2;
+
+// let newCx = pastLeftTopCircle.getAttribute("cx");
+// let newCy = pastLeftTopCircle.getAttribute("cy");
+
+// addSvgCircle(newCx, newCy, 5, "orange");
+
+// rect1.setAttribute("transform-origin", `${newCx} ${newCy}`);
+// rect1.setAttribute("transform", `rotate(${rotate})`);
+
+
+
 
